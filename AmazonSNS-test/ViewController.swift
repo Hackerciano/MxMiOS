cc30871a32862695c7e98024479fd2252c28e070
Diego Graciano <external.DIEGOENRIQUE.GRACIANOGARCIA@mx.bosch.com>
2020-04-03 09:36:15 -0600

    Initial Commit

---
+++ AmazonSNS-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmazonSNS-test
+//
+//  Created by Diego Graciano on 03/04/20.
+//  Copyright © 2020 Diego Graciano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
